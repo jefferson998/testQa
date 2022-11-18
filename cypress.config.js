a4fd1e1660f92bccadd9b1aad6390467c5a2b969
Jefferson Paz <72327620+jefferson998@users.noreply.github.com>
2022-11-18 08:18:35 -0500

    prueba

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  screenshotOnRunFailure:true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    charts: true,
+    reportPageTitle: "test Google",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false
+  },
   e2e: {
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
-
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
